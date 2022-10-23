0c7d4289eab8eededae5d212b9fe320e27e70355
Kevin-C-S <kevincohensolano@gmail.com>
2022-10-22 20:24:46 -0500

    Puede funcionar

--- client.py
+++ client.py
@@ -1,9 +1,11 @@
 import socket
+import select
 
-msgFromClient       = "Hello UDP Server"
+msgFromClient       = "Me mandas el archivo plis?"
 bytesToSend         = str.encode(msgFromClient)
 serverAddressPort   = ("127.0.0.1", 20001)
 bufferSize          = 1024
+timeout = 3
 
 # Create a UDP socket at client side
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -11,8 +13,23 @@ UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 # Send to server using created UDP socket
 UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
+print("Esperando respuesta...")
+
 msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+cliente = "Numero de cliente: {}".format(msgFromServer[0])
+print(cliente)
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+prueba = "Prueba con: {} clientes".format(msgFromServer[0])
+print(prueba)
 
-msg = "Message from Server {}".format(msgFromServer[0])
+f = open(f"Cliente-{cliente}-Prueba{prueba}","wb")
 
-print(msg)
+while True:
+    ready = select.select([UDPClientSocket], [], [], timeout)
+    if ready[0]:
+        data, addr =   UDPClientSocket.recvfrom(1024)
+        f.write(data)
+    else:
+        print("Se ha recibido el archivo")
+        f.close()
+        break
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import socket
+import threading
+import time
 
 localIP     = "127.0.0.1"
 localPort   = 20001
@@ -9,21 +11,13 @@ escAr = 1
 while(centinela):
     try:
         escAr = int(input('Seleccione la opcion 1 para el archivo pequeno (100MB) y 2 para el archivo grande (250MB): '))
-        if escAr == 1 or escAr == 2:
+        conec = input('Cantidad de clientes concurrentes: ')
+        if (escAr == 1 or escAr == 2) and int(conec)>0:
             centinela = False
         else:
             print('Ingrese una opción valida')
     except:
         print('Ingrese una opción valida')
-
-if escAr == 1:
-    msgFromServer = "Hello UDP Client"
-    #msgFromServer = "open("ArchivoPequeno.algo")
-elif escAr == 2:
-    msgFromServer = "Hello UDP Client"
-    #msgFromServer = "open("ArchivoPequeno.algo")
-
-bytesToSend = str.encode(msgFromServer)
 
 # Create a datagram socket
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -31,12 +25,31 @@ UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 # Bind to address and ip
 UDPServerSocket.bind((localIP, localPort))
 
+print("Servidor UDP listo...")
 
-print("UDP server up and listening")
+def handle_client():
+
+    print("Enviando archivo...")
+    if escAr == 1:
+        f = open("Data/Pequeno.txt", 'r')
+    else:
+        f = open("Data/Grande.txt", 'r')
+
+    data = f.read(bufferSize)
+
+    while(data):
+
+        if UDPServerSocket.sendto(str.encode(data), (localIP, localPort)):
+            data = f.read(bufferSize)
+            time.sleep(0.02)
+    f.close()
+    print("Archivo enviado con exito")
+
+i = 1
 
 # Listen for incoming datagrams
 while(True):
-
+    print("Esperando Conexiones")
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
 
     message = bytesAddressPair[0]
@@ -48,6 +61,10 @@ while(True):
     
     print(clientMsg)
     print(clientIP)
-
-    # Sending a reply to client
-    UDPServerSocket.sendto(bytesToSend, address)
+    
+    UDPServerSocket.sendto(str.encode(str(i)), (localIP, localPort))
+    UDPServerSocket.sendto(str.encode(conec), (localIP, localPort))
+    thread = threading.Thread(target=handle_client)
+    thread.start()
+    print(f"[THREADS ACTIVOS] {threading.activeCount() - 1}")
+    i+=1
